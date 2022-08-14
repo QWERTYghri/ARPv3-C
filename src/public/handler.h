151bca3bbf14c56d919ff918fd69584478d51b33
QWERTYbae <nitwitghfj17717@protonmail.com>
2022-08-14 10:30:42 -0700

    commented out handler.h declarations in correspondence to handler.c

--- src/public/handler.h
+++ src/public/handler.h
@@ -13,14 +13,17 @@
 
 /* Redundancy is  pain */
 
+
+/* Note that some instructions are missing because they're similiar to an immediate
+ * addressing but just take direct addressing
+ */
+
 /* Register instructions */
 void LDA ( ARP* lnk );
 void STA ( ARP* lnk );
-void GTA ( ARP* lnk );
 
 void LDX ( ARP* lnk );
 void STX ( ARP* lnk );
-void GTX ( ARP* lnk );
 
 /* Register Transfer */
 void TAX ( ARP* lnk );
@@ -40,9 +43,9 @@ void PAA ( ARP* lnk );
 
 /* Arithemetic */
 void ADD ( ARP* lnk );
-void ADA ( ARP* lnk );
+/* ADA */
 void SUB ( ARP* lnk );
-void SBA ( ARP* lnk );
+/* SBA */
 void INX ( ARP* lnk );
 void DEC ( ARP* lnk );
 void ADX ( ARP* lnk );
@@ -51,8 +54,8 @@ void SUX ( ARP* lnk );
 /* Comparison */
 void CPX ( ARP* lnk );
 void CPA ( ARP* lnk );
-void CXA ( ARP* lnk );
-void CAA ( ARP* lnk );
+/* void CXA ( ARP* lnk ); */
+/* void CAA ( ARP* lnk ); */
 void CLC ( ARP* lnk );
 
 /* Functions / Jumps */
@@ -60,6 +63,5 @@ void JMP ( ARP* lnk );
 void JLC ( ARP* lnk );
 void JMS ( ARP* lnk );
 void RET ( ARP* lnk );
-void HLT ( ARP* lnk );
 
 #endif /* HANDLER_H */
